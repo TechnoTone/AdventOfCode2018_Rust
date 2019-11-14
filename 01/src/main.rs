a4f1e0b196a9a706388856170cb34910c922b786
Tony Hunt <thunt@travelrepublic.co.uk>
2019-11-14 17:35:18 +0000

    01 complete

--- 01/src/main.rs
+++ 01/src/main.rs
@@ -6,30 +6,40 @@ type Result<T> = ::std::result::Result<T, Box<::std::error::Error>>;
 fn main() -> Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
+    let inputs = read_inputs(&input);
 
-    part1(&input)?;
-    part2(&input)?;
+    part1(&inputs)?;
+    part2(&inputs)?;
     Ok(())
 }
 
-fn part1(input: &str) -> Result<()> {
-    let mut freq = 0;
+fn read_inputs(input: &str) -> Vec<i32> {
+    let mut vector: Vec<i32> = vec![];
+
     for line in input.lines() {
-        let change: i32 = line.parse()?;
+        let i: i32 = line.parse().unwrap();
+        vector.push(i);
+    }
+
+    return vector;
+}
+
+fn part1(inputs: &Vec<i32>) -> Result<()> {
+    let mut freq = 0;
+    for change in inputs {
         freq += change;
     }
     writeln!(io::stdout(), "{}", freq)?;
     Ok(())
 }
 
-fn part2(input: &str) -> Result<()> {
+fn part2(inputs: &Vec<i32>) -> Result<()> {
     let mut freq = 0;
     let mut seen = HashSet::new();
     seen.insert(0);
 
     loop {
-        for line in input.lines() {
-            let change: i32 = line.parse()?;
+        for change in inputs {
             freq += change;
             if seen.contains(&freq) {
                 writeln!(io::stdout(), "{}", freq)?;
